14bc65056b734b6355c42b319b8b0bd024c3d78c
Maxime Larrivée-Roy <zmotrin@gmail.com>
2026-02-06 13:29:07 -0500

    Use Chokibasic as file watcher

---
+++ scripts/watch.js
@@ -0,0 +1,43 @@
+const path = require("node:path");
+const { createWatchers, buildCSS, buildJS } = require("chokibasic");
+
+const { close } = createWatchers(
+	[
+		{
+			name: "js",
+			patterns: ["src/scripts/**/*.js"],
+			ignored: ["**/*.min.js"],
+			debounceMs: 150,
+			callback: async (events) => {
+				console.log("[js] batch", events.length, events.map(e => e.file));
+				const entry = path.resolve(__dirname, "../src/scripts/agenda.core.js");
+				const outfile = path.resolve(__dirname, "../src/scripts/agenda.core.min.js");
+				buildJS(entry, outfile);
+			},
+		},
+		{
+			name: "scss",
+			patterns: ["src/styles/**/*.scss"],
+			debounceMs: 150,
+			callback: async (events) => {
+				console.log("[scss] batch", events.length, events.map(e => e.file));
+				const inputScss = path.resolve(__dirname, "../src/styles/agenda.core.scss");
+				const outCssMin = path.resolve(__dirname, "../src/styles/agenda.core.min.css");
+				buildCSS(inputScss, outCssMin);
+			},
+		},
+	],
+	{
+		cwd: process.cwd(),
+		debug: true
+	}
+);
+
+process.on("SIGINT", async () => {
+	await close();
+	process.exit(0);
+});
+
+
+
+
