990d572f2ccee7a99cdba3da1b601d3f57da672d
Maxime Larrivée-Roy <zmotrin@gmail.com>
2026-02-16 18:04:04 -0500

    Add a sitemap and bug fixes

--- dist/scripts/agenda.core.min.js
+++ dist/scripts/agenda.core.min.js
@@ -23,7 +23,7 @@
         =@@@@.     @@@@=                    =@@@@     .@@@@=
 
 
-    Généré: Samedi le 14 février 2026 à 20 h 51
+    Généré: Lundi le 16 février 2026 à 18 h 03
     Auteur: Maxime Larrivée-Roy <mlarriveeroy@gmail.com>
     Github: https://github.com/Agenda-Deux-par-deux/
 
